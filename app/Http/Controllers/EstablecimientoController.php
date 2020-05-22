3e7e35acdf7586895a3fe0c94f9d6cd955640f23
stalinrangel <e.stalinrangel@gmail.com>
2020-05-22 13:07:01 -0430

    chat cliente sin leer

--- app/Http/Controllers/EstablecimientoController.php
+++ app/Http/Controllers/EstablecimientoController.php
@@ -149,12 +149,11 @@ class EstablecimientoController extends Controller
             $Cobros->establecimiento_id = $nuevoEstablecimiento->id;
             $Cobros->usuario_id = $usuario->id;
             $Cobros->observacion = 'Cuota de ingreso al sistema.';*/
-
-                $Notificacion= new \App\Notificaciones_generales;
-                $Notificacion->mensaje= 'Nuevo Proveedor se ha registrado '. $request->input('email'). ' '.$request->input('nombre');
-                $Notificacion->tipo_usuario=3;
-                $Notificacion->ciudad_id=$usuario->ciudad;
+                $Notificacion= new \App\Notificacion;
+                $Notificacion->mensaje='Nuevo Usuario registrado '.$usuario->email;
                 $Notificacion->usuario_id=$usuario->id;
+                $Notificacion->accion=2;
+
                 try {
                     $Notificacion->save();
                 } catch (Exception $e) {
